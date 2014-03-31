158e553d0e177e4386b62e511d9a9a7b04ba1547
te1 <te1@gmail.com>
2014-03-31 10:01:11 +0200

    update autoprefixer
    * bump version to 0.0.7
    * update autoprefixer to 1.1.20140327
    * support for autoprefixer options (e.g ```Visual Cascade```)
    * use autoprefixer default browsers if none are specified

--- index.js
+++ index.js
@@ -9,30 +9,43 @@ var prefix          = require('autoprefixer'),
 
     PLUGIN_NAME     = 'gulp-autoprefixer';
 
-function autoprefixerTransform(opts) {
+function autoprefixerTransform(browsers, options) {
   // Returns a callback that handles the buffered content
   return function(err, buffer, callback) {
     if (err) {
       callback(gutil.PluginError(PLUGIN_NAME, err));
     }
-    var prefixed = prefix(opts).process(String(buffer)).css;
+    var prefixed = prefix(browsers).process(String(buffer), options).css;
     callback(null, new Buffer(prefixed));
   };
 }
 
 function gulpautoprefixer() {
   var stream = new transform({ objectMode: true }),
-      opts = arguments.length > 0 ? [].slice.call(arguments, 0) : ['> 1%','last 2 versions','ff 17','opera 12.1'];  
+      browsers,
+      options;
+
+  if ((arguments.length === 2) && Array.isArray(arguments[0]) && (typeof arguments[1] === 'object') && (arguments[1] !== null)) {
+    browsers = arguments[0];
+    options = arguments[1];
+  } else if ((arguments.length === 1) && Array.isArray(arguments[0])) {
+    browsers = arguments[0];
+  } else if ((arguments.length === 1) && (typeof arguments[0] === 'object') && (arguments[0] !== null)) {
+    options = arguments[0];
+  } else if (arguments.length > 0) {
+    browsers = [].slice.call(arguments, 0);
+  }
+
   stream._transform = function(file, unused, done) {
     // Pass through if null
     if (file.isNull()) {
       stream.push(file);
       done();
       return;
-    } 
+    }
     if (file.isStream()) {
       try {
-        file.contents = file.contents.pipe(new bufferstreams(autoprefixerTransform(opts)));
+        file.contents = file.contents.pipe(new bufferstreams(autoprefixerTransform(browsers, options)));
       } catch (err) {
         err.fileName = file.path;
         stream.emit('error', new gutil.PluginError('gulp-autoprefixer', err));
@@ -41,7 +54,7 @@ function gulpautoprefixer() {
       done();
     } else {
       try {
-        var prefixed = prefix(opts).process(String(file.contents)).css;
+        var prefixed = prefix(browsers).process(String(file.contents), options).css;
         file.contents = new Buffer(prefixed);
       } catch (err) {
         err.fileName = file.path;
@@ -50,7 +63,7 @@ function gulpautoprefixer() {
       stream.push(file);
       done();
     }
-  };  
+  };
   return stream;
 }
 
